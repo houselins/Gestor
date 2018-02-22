5697e326f084ee04e434c093d6e7472dd6d7cb6c
Carlos Casalins Ariza <houselins@gmail.com>
2018-02-22 13:43:13 -0500

    Update Nodo.java

--- src/laboratorio/i/datos/ii/Nodo.java
+++ src/laboratorio/i/datos/ii/Nodo.java
@@ -11,13 +11,48 @@ package laboratorio.i.datos.ii;
  */
 public class Nodo {
     
-    
+    Boolean Paquete;//si es un paquete va a ser verdadero
+    private String nombre;
     private String data;
     private Nodo rLink;
     private Nodo lLink;
 
-    public Nodo() {
-        //habia una vez un barquito chiquitito
+    public Nodo(String nombre) {
+        this.nombre = nombre;
+    }
+    /*
+    void add(int i) {
+        Nodo temp=this,ant=null;
+        while(temp!=null){
+            if(temp.data==i)break;
+            else if(i>temp.data){
+                ant=temp;
+                temp=temp.Rlink;
+            }else{
+                ant=temp;
+                temp=temp.Llink;
+            }
+        }
+        if(temp==null){//no esta repetido
+            if(i>ant.data) ant.rLink= new Nodo(i);
+            else ant.Llink= new Nodo(i);
+        }
+    }
+    */
+    void add(String nombre, Boolean paquete){
+        if (paquete) {
+            this.ultPaquete().lLink=new Nodo(nombre);
+        }else{
+            
+        }
+        
+    }
+    private Nodo ultPaquete(){
+        Nodo p=this;
+        while(p.lLink != null){
+            p=p.lLink;
+        }
+        return p;
     }
     
 }
